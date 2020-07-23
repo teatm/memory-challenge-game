4b70a7f50f4d3fbab3ff3411629e28845a996beb
Albert <albert@caulis.jp>
2020-07-23 22:06:42 +0900

    Add time remaining countdown with useEffect

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import utils from '../math-utils'
 import PlayCard from './PlayCard';
 
 const randomArray = (utils.range(1, 12));
-
 const shuffled = randomArray.sort(() => 0.5 - Math.random());
 
-
 const Game = () => {
+    const [secondsLeft, setSecondsLeft] = useState(10);
+
+    useEffect(() => {
+        if (secondsLeft > 0) {
+            const timerId = setTimeout(
+                () => setSecondsLeft((prevSecondsLeft) => prevSecondsLeft - 1),
+                1000
+            );
+            return () => clearTimeout(timerId);
+        }
+    }, [secondsLeft]);
+
     return (
         <div className="right">
             {shuffled.map((number) => (
@@ -18,6 +28,7 @@ const Game = () => {
                     status={number % 3 == 0 ? 'used' : number % 3 == 1 ? 'available' : 'wrong'}
                 />
             ))}
+            <div className="timer">Time Remaining: {secondsLeft}</div>
         </div>
     );
 };
